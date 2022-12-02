f0f41d4ee6eb228fdd622f786ae77a95da243865
Billy Li Asus Zenbook <egoistheresy@outlook.com>
2022-12-02 16:40:12 -0600

    20221202 Billy Li incorrect bullet logic

--- sketch.js
+++ sketch.js
@@ -12,18 +12,30 @@ let MAXSPEED = 8;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  setUpSpaceShip();
+  // set up the spaceship
+  spaceship = new Sprite(); // create the main character
+  spaceship.rotation = -90; // adjust the rotation direction
+
+  // set up the bullets 
+  bullets = new Group(); // create a group of bullet
 }
 
 function draw() {
 
   spaceShipControl();    // take charge of the control block of the spaceship
   spaceshipResetPosit(); // reset the position of the spaceship if out of the boundary
+  // bullet shooting logic         
+  if (kb.presses('J')) {
+    // create sprites
+    new bullets.Sprite(spaceship.position.x, spaceship.position.y, 5);
+    bullets.setSpeed(16, spaceship.rotation);
+  }
+
 
 
   // player.overlap(coins, collect);
   // drawSprites();
-  background(0);
+  background(0);        //background to be set dark
 }
 
 
@@ -42,32 +54,30 @@ function spaceShipControl() {
     spaceship.direction += 1.5;
   }
 
-  if (kb.pressing('up') || kb.pressing('W')) {
-    spaceship.addSpeed(0.1, spaceship.rotation);
+  if ((kb.pressing('up') || kb.pressing('W'))) {
+    if (spaceship.speed < MAXSPEED) {
+      spaceship.addSpeed(0.1, spaceship.rotation);
+    } else {
+      spaceship.speed == MAXSPEED;
+    }
   } else {
-    spaceship.speed = (spaceship.speed > 0) ? spaceship.speed - 0.1 : 0; 
+    spaceship.speed = (spaceship.speed > 0) ? spaceship.speed - 0.1 : 0;
   }
 }
 
+function spaceshipResetPosit() {
+  for (let i = 0; i < allSprites.length; i++) {
+    let s = allSprites[i];
+    if (s.position.x < -MARGIN) {
+      s.position.x = width + MARGIN;
+    } else if (s.position.x > width + MARGIN) {
+      s.position.x = -MARGIN;
+    }
 
-  function setUpSpaceShip() {
-    spaceship = new Sprite(); // create the main character
-    spaceship.rotation = -90; // adjust the rotation direction
-  }
-
-  function spaceshipResetPosit() {
-    for (let i = 0; i < allSprites.length; i++) {
-      let s = allSprites[i];
-      if (s.position.x < -MARGIN) {
-        s.position.x = width + MARGIN;
-      } else if (s.position.x > width + MARGIN) {
-        s.position.x = -MARGIN;
-      }
-
-      if (s.position.y < -MARGIN) {
-        s.position.y = height + MARGIN;
-      } else if (s.position.y > height + MARGIN) {
-        s.position.y = -MARGIN;
-      }
+    if (s.position.y < -MARGIN) {
+      s.position.y = height + MARGIN;
+    } else if (s.position.y > height + MARGIN) {
+      s.position.y = -MARGIN;
     }
   }
+}
