5fe7231f56352708edcfa9c55940f0c48340e3f6
ezel <egoistheresy@outlook.com>
2022-12-01 23:20:40 -0600

    20221201 1120 Smooth Spaceship movement

--- sketch.js
+++ sketch.js
@@ -6,7 +6,8 @@ let asteroids; // asteroids in the world
 let bullets; // the group of bullets
 
 // some constants
-let MARGIN = 42;
+let MARGIN = 40;
+let MAXSPEED = 8;
 
 
 function setup() {
@@ -15,9 +16,9 @@ function setup() {
 }
 
 function draw() {
-  spaceShipControl();
-  // spaceShipBoundaryCond();
 
+  spaceShipControl();    // take charge of the control block of the spaceship
+  spaceshipResetPosit(); // reset the position of the spaceship if out of the boundary
 
 
   // player.overlap(coins, collect);
@@ -26,26 +27,47 @@ function draw() {
 }
 
 
-function spaceShipControl(){
-    // control the player movement
-    if (kb.presses('left') || kb.presses('A')) {
-      // (direction, speed, distance)
-      spaceship.rotation  -= 16;
-    }
-    
-    if (kb.presses('right') || kb.presses('D')) {
-      // (direction, speed, distance)
-      spaceship.rotation += 16;
-    } 
-    
-    if(kb.pressing('up') || kb.pressing('W')){
-      spaceship.addSpeed(0.5, spaceship.rotation);
-    }else{
-      spaceship.speed = 0;
-    }
+
+function spaceShipControl() {
+  // control the player movement
+  if (kb.presses('left') || kb.presses('A') || kb.pressing('left') || kb.pressing('A')) {
+    // (direction, speed, distance)
+    spaceship.rotation -= 1.5;
+    spaceship.direction -= 1.5;
+  }
+
+  if (kb.presses('right') || kb.presses('D') || kb.pressing('right') || kb.pressing('D')) {
+    // (direction, speed, distance)
+    spaceship.rotation += 1.5;
+    spaceship.direction += 1.5;
+  }
+
+  if (kb.pressing('up') || kb.pressing('W')) {
+    spaceship.addSpeed(0.1, spaceship.rotation);
+  } else {
+    spaceship.speed = (spaceship.speed > 0) ? spaceship.speed - 0.1 : 0; 
+  }
 }
 
-function setUpSpaceShip(){
-  spaceship = new Sprite(); // create the main character
-  spaceship.rotation = -90; // adjust the rotation direction
-}
+
+  function setUpSpaceShip() {
+    spaceship = new Sprite(); // create the main character
+    spaceship.rotation = -90; // adjust the rotation direction
+  }
+
+  function spaceshipResetPosit() {
+    for (let i = 0; i < allSprites.length; i++) {
+      let s = allSprites[i];
+      if (s.position.x < -MARGIN) {
+        s.position.x = width + MARGIN;
+      } else if (s.position.x > width + MARGIN) {
+        s.position.x = -MARGIN;
+      }
+
+      if (s.position.y < -MARGIN) {
+        s.position.y = height + MARGIN;
+      } else if (s.position.y > height + MARGIN) {
+        s.position.y = -MARGIN;
+      }
+    }
+  }
